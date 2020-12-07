f528c07121e5a5d5b5e0dee3439cc522b603c539
nsh12 <ansh120022@gmail.com>
2020-12-07 15:58:34 +0300

    Домашнее задание на темы "Списки" и "Рекурсия" Лунные кратеры

---
+++ Craters/main.py
@@ -0,0 +1,51 @@
+"""Поиск групп единиц в файле."""
+import numpy as np
+import os
+import sys
+
+
+def read_file(file: str) -> bytearray:
+    """Чтение из файла c обработкой негативных случаев."""
+    try:
+        matrix = np.loadtxt(file, dtype=int)
+        if os.stat(file).st_size == 0:
+            print("Файл пустой")
+            sys.exit(0)
+        return matrix
+    except IOError:
+        print("Входной файл отсутствует.")
+        sys.exit(0)
+
+
+def find_crater(matrix: np.ndarray, i: int, j: int) -> bool:
+    """Рекурсивный поиск кратера."""
+    if i < 0 or i >= matrix.shape[0]:
+        return False
+    if j < 0 or j >= matrix.shape[1]:
+        return False
+    crater = matrix[i][j] == 1
+    matrix[i][j] = 0
+    if crater:
+        find_crater(matrix, i, j + 1)
+        find_crater(matrix, i, j - 1)
+        find_crater(matrix, i + 1, j)
+        find_crater(matrix, i - 1, j)
+    return crater
+
+
+def calculate(array: np.ndarray) -> int:
+    """Учёт найденных кратеров."""
+    craters = 0
+    for i in range(array.shape[1]):
+        for j in range(array.shape[0]):
+            if find_crater(array, i, j):
+                craters += 1
+    return craters
+
+
+input_array = read_file("input_file.txt")
+if np.sum(input_array) == 0:
+    print(f"Здесь кратеров нет.")
+else:
+    result = calculate(input_array)
+    print(f"Найдено кратеров: {result}")
---
+++ Craters/unit_test.py
@@ -0,0 +1,19 @@
+import unittest
+from main import (
+    read_file,
+    calculate
+)
+
+
+class FunctionalChecker(unittest.TestCase):
+    def test_calculate(self):
+        self.assertEqual(calculate(read_file("test_files/1.txt")), 1)
+        self.assertEqual(calculate(read_file("test_files/2.txt")), 1)
+        self.assertEqual(calculate(read_file("test_files/3.txt")), 2)
+        self.assertEqual(calculate(read_file("test_files/4.txt")), 3)
+        self.assertEqual(calculate(read_file("test_files/6.txt")), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
