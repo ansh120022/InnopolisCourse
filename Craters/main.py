57cd6269b5462cfe6483d76b00dd24bee3330fb6
nsh12 <ansh120022@gmail.com>
2020-12-12 21:03:12 +0300

    Tuned to tox tests

--- Craters/main.py
+++ Craters/main.py
@@ -4,14 +4,14 @@ import os
 import sys
 
 
-def read_file(file: str) -> bytearray:
+def read_file(file: str) -> list:
     """Чтение из файла c обработкой негативных случаев."""
     try:
         matrix = np.loadtxt(file, dtype=int)
         if os.stat(file).st_size == 0:
             print("Файл пустой")
             sys.exit(0)
-        return matrix
+        return matrix.tolist()
     except IOError:
         print("Входной файл отсутствует.")
         sys.exit(0)
@@ -36,17 +36,17 @@ def find_crater(matrix: np.ndarray, i: int, j: int) -> bool:
 def calculate(array: list) -> int:
     """Учёт найденных кратеров."""
     craters = 0
-    array = np.asarray(array)
-    for i in range(array.shape[1]):
-        for j in range(array.shape[0]):
-            if find_crater(array, i, j):
+    matrix = np.asarray(array)
+    for i in range(matrix.shape[1]):
+        for j in range(matrix.shape[0]):
+            if find_crater(matrix, i, j):
                 craters += 1
     return craters
 
 
 input_array = read_file("input_file.txt")
-if np.sum(input_array) == 0:
-    print(f"Здесь кратеров нет.")
+if not any(input_array):
+    print("Здесь кратеров нет.")
 else:
     result = calculate(input_array)
     print(f"Найдено кратеров: {result}")
